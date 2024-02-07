248bcac2e0efb6060c1637f8b89ace95467a2258
İrem Akın Tunga <iremakin00@gmail.com>
2024-02-07 22:31:03 +0300

    Completes ArrayList Challenge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import model.GroceryList;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        GroceryList groceryList = new GroceryList();
+        int choice;
+        do {
+            System.out.println("Yapılmak istenen operasyonu seçin(0: quit, 1: ekleme, 2: çıkarma)");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("eklenmesini istedğiniz elamanları giriniz.(birden fazlaysa virgül kullanın)");
+                    String itemsToAdd = scanner.nextLine();
+                    groceryList.addItems(itemsToAdd);
+                    break;
+                case 2:
+                    System.out.println("silinmesini istedğiniz elamanları giriniz.(birden fazlaysa virgül kullanın)");
+                    String itemsToRemove = scanner.nextLine();
+                    groceryList.removeItems(itemsToRemove);
+                    break;
+            }
+            groceryList.printSorted();
+        } while (choice != 0);
+        scanner.close();
+
     }
 }
